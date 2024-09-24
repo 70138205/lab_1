0c369b1d1308c38a238986272f4ed4e6a6bb8048
70138205 <158143835+70138205@users.noreply.github.com>
2024-09-23 22:51:25 -0700

    first commit

---
+++ bin/lab_1.dart
@@ -0,0 +1,8 @@
+import 'package:lab_1/lab_1.dart' as lab_1;
+
+void main() {
+  String doubleVariable ="10.5";
+  print("${doubleVariable.length} is a double variable");
+  print('Lab 01 Dart Installation');
+
+}
