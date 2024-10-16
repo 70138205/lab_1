c8a42b9b8bbe99a0a69af6e677ed52ef624114d6
70138205 <158143835+70138205@users.noreply.github.com>
2024-10-15 22:14:22 -0700

    aa

--- bin/lab_1.dart
+++ bin/lab_1.dart
@@ -1,8 +1,6 @@
-import 'package:lab_1/lab_1.dart' as lab_1;
 
 void main() {
-  String doubleVariable ="10.5";
+  String doubleVariable = "10.5";
   print("${doubleVariable.length} is a double variable");
   print('Lab 01 Dart Installation');
-
 }
